ec35159235168ff321bcb618758c41d3f80f14df
gladyne <kiseyryouta@gmail.com>
2022-09-04 14:19:40 +0700

    intializing chashier tekaja

---
+++ android/app/src/main/kotlin/com/example/cashier_tekaja/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cashier_tekaja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Chashier Tekaja",
+      home: _HomePage(),
+    );
+  }
+}
+
+class _HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chasier APP"),
+      ),
+    );
+  }
+}
